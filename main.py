53111c085afe81967b27db576efdbb5461d98c37
Hassan <ihassan1@kpmg.com.au>
2018-01-31 22:40:51 +1100

    adding hello azure

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "hello world"
+  a = word_tokenize("somthing is awesome")
+  return a[0]
 
 if __name__ == '__main__':
   app.run(debug=True)

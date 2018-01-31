60d6be98334b499e5b6f017d87b9397d1e7d46c4
Hassan <ihassan1@kpmg.com.au>
2018-01-31 22:20:23 +1100

    adding nltk

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from nltk import word_tokenize
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Azure!'
+  a = word_tokenize("how are you man")
+  return a[0]
 
 if __name__ == '__main__':
   app.run()
